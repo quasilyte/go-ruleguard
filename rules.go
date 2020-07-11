a65f81a159c7016878882e6cafcd58b267ff655e
Oleg Kovalov <iamolegkovalov@gmail.com>
2020-07-11 19:57:33 +0200

    rules.go: add suggestion to bits rules (#55)

--- rules.go
+++ rules.go
@@ -92,24 +92,42 @@ func _(m fluent.Matcher) {
 
 // See https://twitter.com/dgryski/status/1281348103505768449
 func useMathBits(m fluent.Matcher) {
-	// TODO: rewrite with Suggest templates so the quickfix is possible?
-	m.Match(`$x >> $n | $x << (8 - $n)`,
+	// RotateLeft
+	m.Match(
 		`$x << $n | $x >> (8 - $n)`,
-		`$x >> (8 - $n) | $x << $n`,
-		`$x << (8 - $n) | $x >> $n`,
-		`$x >> $n | $x << (16 - $n)`,
+		`$x >> (8 - $n) | $x << $n`).
+		Suggest(`bits.RotateLeft8($x, $n)`)
+
+	m.Match(
 		`$x << $n | $x >> (16 - $n)`,
-		`$x >> (16 - $n) | $x << $n`,
-		`$x << (16 - $n) | $x >> $n`,
-		`$x >> $n | $x << (32 - $n)`,
+		`$x >> (16 - $n) | $x << $n`).
+		Suggest(`bits.RotateLeft16($x, $n)`)
+	m.Match(
 		`$x << $n | $x >> (32 - $n)`,
-		`$x >> (32 - $n) | $x << $n`,
-		`$x << (32 - $n) | $x >> $n`,
-		`$x >> $n | $x << (64 - $n)`,
+		`$x >> (32 - $n) | $x << $n`).
+		Suggest(`bits.RotateLeft32($x, $n)`)
+	m.Match(
 		`$x << $n | $x >> (64 - $n)`,
-		`$x >> (64 - $n) | $x << $n`,
+		`$x >> (64 - $n) | $x << $n`).
+		Suggest(`bits.RotateLeft64($x, $n)`)
+
+	// RotateRight (from docs: To rotate x right by k bits, call RotateLeftX(x, -k))
+	m.Match(
+		`$x >> $n | $x << (8 - $n)`,
+		`$x << (8 - $n) | $x >> $n`).
+		Suggest(`bits.RotateLeft8($x, -$n)`)
+	m.Match(
+		`$x >> $n | $x << (16 - $n)`,
+		`$x << (16 - $n) | $x >> $n`).
+		Suggest(`bits.RotateLeft16($x, -$n)`)
+	m.Match(
+		`$x >> $n | $x << (32 - $n)`,
+		`$x << (32 - $n) | $x >> $n`).
+		Suggest(`bits.RotateLeft32($x, -$n)`)
+	m.Match(
+		`$x >> $n | $x << (64 - $n)`,
 		`$x << (64 - $n) | $x >> $n`).
-		Report(`try using math/bits instead`)
+		Suggest(`bits.RotateLeft64($x, -$n)`)
 }
 
 func gocriticWrapperFunc(m fluent.Matcher) {
