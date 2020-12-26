a7b79518656cbf17a53675e326e7ec17c6110bf2
Iskander Sharipov <quasilyte@gmail.com>
2020-12-26 11:09:55 +0300

    ruleguard: add `go list` fallback to the goImporter

--- ruleguard/importer.go
+++ ruleguard/importer.go
@@ -1,42 +1,88 @@
 package ruleguard
 
 import (
+	"go/ast"
 	"go/importer"
+	"go/parser"
 	"go/token"
 	"go/types"
+	"path/filepath"
+
+	"github.com/quasilyte/go-ruleguard/internal/golist"
 )
 
+// goImporter is a `types.Importer` that tries to load a package no matter what.
+// It iterates through multiple import strategies and accepts whatever succeeds first.
 type goImporter struct {
 	// TODO(quasilyte): share importers with gogrep?
 
-	gcImporter  types.Importer
-	srcImporter types.Importer
+	// cache contains all imported packages, from any importer.
+	// Both default and source importers have their own caches,
+	// but since we use several importers, it's better to
+	// have our own, unified cache.
+	cache map[string]*types.Package
+
+	fset *token.FileSet
+
+	defaultImporter types.Importer
+	srcImporter     types.Importer
 }
 
 func newGoImporter(fset *token.FileSet) *goImporter {
 	return &goImporter{
-		gcImporter:  importer.Default(),
-		srcImporter: importer.ForCompiler(fset, "source", nil),
+		cache:           make(map[string]*types.Package),
+		fset:            fset,
+		defaultImporter: importer.Default(),
+		srcImporter:     importer.ForCompiler(fset, "source", nil),
 	}
 }
 
 func (imp *goImporter) Import(path string) (*types.Package, error) {
-	pkg, err := imp.srcImporter.Import(path)
-	if err == nil {
+	if pkg := imp.cache[path]; pkg != nil {
 		return pkg, nil
 	}
-	return imp.gcImporter.Import(path)
+
+	pkg, err1 := imp.defaultImporter.Import(path)
+	if err1 == nil {
+		imp.cache[path] = pkg
+		return pkg, nil
+	}
+
+	pkg, err2 := imp.srcImporter.Import(path)
+	if err2 == nil {
+		imp.cache[path] = pkg
+		return pkg, nil
+	}
+
+	// Fallback to `go list` as a last resort.
+	pkg, err3 := imp.golistImport(path)
+	if err3 == nil {
+		imp.cache[path] = pkg
+		return pkg, nil
+	}
+
+	return nil, err1
 }
 
-func (imp *goImporter) ImportFrom(path, dir string, mode types.ImportMode) (*types.Package, error) {
-	if srcImporter, ok := imp.srcImporter.(types.ImporterFrom); ok {
-		pkg, err := srcImporter.ImportFrom(path, dir, mode)
-		if err == nil {
-			return pkg, nil
+func (imp *goImporter) golistImport(path string) (*types.Package, error) {
+	golistPkg, err := golist.JSON(path)
+	if err != nil {
+		return nil, err
+	}
+
+	files := make([]*ast.File, 0, len(golistPkg.GoFiles))
+	for _, filename := range golistPkg.GoFiles {
+		fullname := filepath.Join(golistPkg.Dir, filename)
+		f, err := parser.ParseFile(imp.fset, fullname, nil, 0)
+		if err != nil {
+			return nil, err
 		}
+		files = append(files, f)
 	}
-	if gcImporter, ok := imp.gcImporter.(types.ImporterFrom); ok {
-		return gcImporter.ImportFrom(path, dir, mode)
-	}
-	return imp.Import(path)
+
+	// TODO: do we want to assign imp as importer for this nested typecherker?
+	// Otherwise it won't be able to resolve imports.
+	var typecheker types.Config
+	var info types.Info
+	return typecheker.Check(path, imp.fset, files, &info)
 }
