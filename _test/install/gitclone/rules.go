d8abdb33e900f13013dee8a968d3913a96186ac9
Iskander Sharipov <quasilyte@gmail.com>
2021-01-08 07:09:41 +0300

    _test/install: run ruleguard on GOROOT
    For `git clone` version, also use `-race` mode.

--- _test/install/gitclone/rules.go
+++ _test/install/gitclone/rules.go
@@ -4,10 +4,23 @@ package gorules
 
 import (
 	"github.com/quasilyte/go-ruleguard/dsl"
+	"github.com/quasilyte/go-ruleguard/dsl/types"
 )
+
+func implementsStringer(ctx *dsl.VarFilterContext) bool {
+	stringer := ctx.GetInterface(`fmt.Stringer`)
+	return types.Implements(ctx.Type, stringer) ||
+		types.Implements(types.NewPointer(ctx.Type), stringer)
+}
 
 func exprUnparen(m dsl.Matcher) {
 	m.Match(`$f($*_, ($x), $*_)`).
 		Report(`the parentheses around $x are superfluous`).
 		Suggest(`$f($x)`)
+}
+
+func sprintStringer(m dsl.Matcher) {
+	m.Match(`fmt.Sprint($x)`).
+		Where(m["x"].Filter(implementsStringer) && m["x"].Addressable).
+		Report(`can use $x.String() directly`)
 }
