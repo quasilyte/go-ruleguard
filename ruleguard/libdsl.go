7968289708448cdc8ea2fa21d0db203a75d93994
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-05-18 19:08:37 +0300

    ruleguard: implement `dsl/types` Struct type and its methods (#393)

--- ruleguard/libdsl.go
+++ ruleguard/libdsl.go
@@ -36,8 +36,10 @@ func initEnv(state *engineState, env *quasigo.Env) {
 		`github.com/quasilyte/go-ruleguard/dsl/types.Type`:        dslTypesType{},
 		`*github.com/quasilyte/go-ruleguard/dsl/types.Interface`:  dslTypesInterface{},
 		`*github.com/quasilyte/go-ruleguard/dsl/types.Pointer`:    dslTypesPointer{},
+		`*github.com/quasilyte/go-ruleguard/dsl/types.Struct`:     dslTypesStruct{},
 		`*github.com/quasilyte/go-ruleguard/dsl/types.Array`:      dslTypesArray{},
 		`*github.com/quasilyte/go-ruleguard/dsl/types.Slice`:      dslTypesSlice{},
+		`*github.com/quasilyte/go-ruleguard/dsl/types.Var`:        dslTypesVar{},
 	}
 
 	for qualifier, typ := range nativeTypes {
@@ -166,6 +168,35 @@ func (dslTypesPointer) Elem(stack *quasigo.ValueStack) {
 	stack.Push(stack.Pop().(*types.Pointer).Elem())
 }
 
+type dslTypesStruct struct{}
+
+func (native dslTypesStruct) funcs() map[string]func(*quasigo.ValueStack) {
+	return map[string]func(*quasigo.ValueStack){
+		"Underlying": native.Underlying,
+		"String":     native.String,
+		"NumFields":  native.NumFields,
+		"Field":      native.Field,
+	}
+}
+
+func (dslTypesStruct) Underlying(stack *quasigo.ValueStack) {
+	stack.Push(stack.Pop().(*types.Struct).Underlying())
+}
+
+func (dslTypesStruct) String(stack *quasigo.ValueStack) {
+	stack.Push(stack.Pop().(*types.Struct).String())
+}
+
+func (dslTypesStruct) NumFields(stack *quasigo.ValueStack) {
+	stack.PushInt(stack.Pop().(*types.Struct).NumFields())
+}
+
+func (dslTypesStruct) Field(stack *quasigo.ValueStack) {
+	i := stack.PopInt()
+	typ := stack.Pop().(*types.Struct)
+	stack.Push(typ.Field(i))
+}
+
 type dslTypesPackage struct{}
 
 func (native dslTypesPackage) funcs() map[string]func(*quasigo.ValueStack) {
@@ -179,6 +210,7 @@ func (native dslTypesPackage) funcs() map[string]func(*quasigo.ValueStack) {
 		"AsSlice":     native.AsSlice,
 		"AsPointer":   native.AsPointer,
 		"AsInterface": native.AsInterface,
+		"AsStruct":    native.AsStruct,
 	}
 }
 
@@ -228,6 +260,28 @@ func (dslTypesPackage) AsPointer(stack *quasigo.ValueStack) {
 func (dslTypesPackage) AsInterface(stack *quasigo.ValueStack) {
 	typ, _ := stack.Pop().(types.Type).(*types.Interface)
 	stack.Push(typ)
+}
+
+func (dslTypesPackage) AsStruct(stack *quasigo.ValueStack) {
+	typ, _ := stack.Pop().(types.Type).(*types.Struct)
+	stack.Push(typ)
+}
+
+type dslTypesVar struct{}
+
+func (native dslTypesVar) funcs() map[string]func(*quasigo.ValueStack) {
+	return map[string]func(*quasigo.ValueStack){
+		"Embedded": native.Embedded,
+		"Type":     native.Type,
+	}
+}
+
+func (dslTypesVar) Embedded(stack *quasigo.ValueStack) {
+	stack.Push(stack.Pop().(*types.Var).Embedded())
+}
+
+func (dslTypesVar) Type(stack *quasigo.ValueStack) {
+	stack.Push(stack.Pop().(*types.Var).Type())
 }
 
 type dslDoContext struct{}
