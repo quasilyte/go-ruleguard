0448b915a3537c01aae74c7203ee13f27e506a66
Iskander Sharipov <quasilyte@gmail.com>
2022-05-18 19:00:39 +0300

    dsl/types: add String+Underlying methods to Struct

--- dsl/types/type_impl.go
+++ dsl/types/type_impl.go
@@ -8,8 +8,10 @@ func (*Array) String() string     { return "" }
 func (*Slice) String() string     { return "" }
 func (*Pointer) String() string   { return "" }
 func (*Interface) String() string { return "" }
+func (*Struct) String() string    { return "" }
 
 func (*Array) Underlying() Type     { return nil }
 func (*Slice) Underlying() Type     { return nil }
 func (*Pointer) Underlying() Type   { return nil }
 func (*Interface) Underlying() Type { return nil }
+func (*Struct) Underlying() Type    { return nil }
