ef3f441e7ce7bc988c51a288d6be514fb9735513
Iskander Sharipov <quasilyte@gmail.com>
2022-09-10 09:50:26 +0300

    ruleguard: fix sizeof filter for type params
    Extended the dsl documentation to reflect the change:
    
    	For expressions of unknown size, like type params in generics,
    	any filter using this operand will fail.
    
    We're not using 0 as a fallback size to avoid the confusion
    with zero-sized values like `struct{}`.
    
    See https://github.com/go-critic/go-critic/pull/1237
    
    Fixes #406

--- analyzer/testdata/src/generics/rules.go
+++ analyzer/testdata/src/generics/rules.go
@@ -10,3 +10,11 @@ func externalErrorReassign(m dsl.Matcher) {
 		Where(m["err"].Type.Is(`error`) && m["pkg"].Object.Is(`PkgName`)).
 		Report(`suspicious reassigment of error from another package`)
 }
+
+func largeLoopCopy(m dsl.Matcher) {
+	m.Match(
+		`for $_, $v := range $_ { $*_ }`,
+	).
+		Where(m["v"].Type.Size > 512).
+		Report(`loop copies large value each iteration`)
+}
