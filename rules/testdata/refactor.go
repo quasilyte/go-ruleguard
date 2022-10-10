ce1bf37a2cf8b2ccf5af3970064200a576ebf24c
Tom <github@junk1tm.me>
2022-10-10 11:39:10 +0400

    rules: add sort.Float64s to sortFuncs (#418)

--- rules/testdata/refactor.go
+++ rules/testdata/refactor.go
@@ -7,6 +7,7 @@ import (
 func sortFuncs() {
 	var ints []int
 	var strs []string
+	var floats []float64
 
 	sort.Slice(ints, func(i, j int) bool { // want `\QsortFuncs: suggestion: sort.Ints(ints)`
 		return ints[i] < ints[j]
@@ -14,5 +15,9 @@ func sortFuncs() {
 
 	sort.Slice(strs, func(i, j int) bool { // want `\QsortFuncs: suggestion: sort.Strings(strs)`
 		return strs[i] < strs[j]
+	})
+
+	sort.Slice(floats, func(i, j int) bool { // want `\QsortFuncs: suggestion: sort.Float64s(floats)`
+		return floats[i] < floats[j]
 	})
 }
