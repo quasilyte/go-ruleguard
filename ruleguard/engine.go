17e5f22037c3fa888ff28aaf7798c0a13b7ce103
Jordan Liggitt <liggitt@google.com>
2023-08-09 16:53:02 -0400

    Make go env vars static

--- ruleguard/engine.go
+++ ruleguard/engine.go
@@ -248,7 +248,7 @@ func inferBuildContext() *build.Context {
 	// Inherit most fields from the build.Default.
 	ctx := build.Default
 
-	env, err := goenv.Read([]string{"GOROOT", "GOPATH", "GOARCH", "GOOS", "CGO_ENABLED"})
+	env, err := goenv.Read()
 	if err != nil {
 		return &ctx
 	}
